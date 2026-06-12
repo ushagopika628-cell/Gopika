6408ff0329673745c24be3aa7f840faa958796eb
ushagopika628 <ushagopika628@gmail.com>
2026-06-12 15:41:22 +0530

    function.js

---
+++ datatype.js
@@ -0,0 +1,51 @@
+console.log("Welcome to primitive datatype");
+
+//Numbers
+console.log("DT Numbers");
+var age = 30;
+console.log(30);
+
+
+//strings
+console.log("DT String");
+var name = "John";
+console.log(name);
+
+//Boolean
+console.log("DT Boolean");
+var student = true;
+console.log(student);
+
+//Null
+console.log("DT Null");
+var emptyvalue = null;
+console.log(emptyvalue);
+
+//Undefined
+console.log("DT Undefined");
+var undefinedvalue;
+console.log(undefinedvalue);
+
+//Symbol
+console.log("DT Symbol");
+var symbolvalue = symbol('symbol');
+console.log(symbolvalue);
+
+console.log("Welcome to non-primitive datatype");
+
+//Object
+console.log("DT Object");
+var person = {
+    name : "John"
+    age  : 30   
+};
+console.log(person.age);
+
+//Arrays
+console.log("DT Arrays");
+var numbers = ["Kiwi","fig","pine","apple","orange"];
+console.log(numbers.length);
+console.log(numbers[4]);
+
+
+
---
+++ function.js
@@ -0,0 +1,12 @@
+//non paramatic functions
+function greet() {
+     console.log("Hello,World");
+}
+greet();
+
+//paramatic functions
+function add(a,b)
+{
+    return a+b ;
+}
+console.log(add(5,3));
