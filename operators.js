2c74770756f8424335cb5578eb2c07a4af346a2d
ushagopika628 <ushagopika628@gmail.com>
2026-06-12 10:59:50 +0530

    operators.js

---
+++ operators.js
@@ -0,0 +1,24 @@
+//Arithmatic
+var a = 20;
+var b = 10;
+console.log(a+b)//addition
+
+var a = 20;
+var b = 10;
+console.log(a-b)//Subtraction
+
+var a = 20;
+var b = 10;
+console.log(a*b)//Multiplication
+
+var a = 20;
+var b = 10;
+console.log(a/b)//Division
+
+var a = 20;
+var b = 10;
+console.log(a%b)//Modules
+
+var a = 20;
+var b = 10;
+console.log(a**b)//Expotation 
