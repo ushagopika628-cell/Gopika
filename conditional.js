9929ec8c5bdbbfd8744b983517e859b616b62e58
ushagopika628 <ushagopika628@gmail.com>
2026-06-12 16:02:30 +0530

    conditional.js

---
+++ conditional.js
@@ -0,0 +1,15 @@
+var light = "White"
+
+if(light == "Green"){
+    console.log("Go!");
+}
+else if(light == "Yellow"){
+    console.log("Wait!");
+}
+else if(light == "Red"){
+    console.log("Stop!");
+}
+else{
+    console.log("Invalid light");
+}
+console.log("Outside if");
