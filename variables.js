8aa799d536a8efea119381a24eeb024d31b69fe2
ushagopika628 <ushagopika628@gmail.com>
2026-06-12 10:23:10 +0530

    variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+// variables using var
+var name = "VETIAS"
+console.log(name)
+
+//variables using let
+let age = 19
+console.log(age)
+
+//variables using const 
+const country = "India"
+console.log(country)
